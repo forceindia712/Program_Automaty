4b3ea0aef8f4e9a45ff5b09967af23eee9dc5c2f
forceindia712 <80550704+forceindia712@users.noreply.github.com>
2022-01-19 11:02:02 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿
+namespace AutomatProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Check = new System.Windows.Forms.Button();
+            this.Sprawdz = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(448, 482);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(318, 519);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(362, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Automat akceptujący słowa nad alfabetem {0,1} zawierające podsłowo 00.";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Check
+            // 
+            this.Check.Location = new System.Drawing.Point(868, 479);
+            this.Check.Name = "Check";
+            this.Check.Size = new System.Drawing.Size(75, 23);
+            this.Check.TabIndex = 2;
+            this.Check.Text = "Check";
+            this.Check.UseVisualStyleBackColor = true;
+            this.Check.Click += new System.EventHandler(this.Check_Click);
+            // 
+            // Sprawdz
+            // 
+            this.Sprawdz.AutoSize = true;
+            this.Sprawdz.Location = new System.Drawing.Point(887, 519);
+            this.Sprawdz.Name = "Sprawdz";
+            this.Sprawdz.Size = new System.Drawing.Size(89, 13);
+            this.Sprawdz.TabIndex = 3;
+            this.Sprawdz.Text = "Check";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(130, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "A";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(264, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "B";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(400, 169);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(14, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "C";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(154, 207);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(197, 260);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(197, 158);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(326, 158);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(13, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "0";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(373, 86);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(75, 53);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(411, 70);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(13, 13);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "1";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(398, 70);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(16, 13);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "0,";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(101, 95);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(75, 53);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 14;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(131, 77);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(13, 13);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;    
+            this.ClientSize = new System.Drawing.Size(1037, 564);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Sprawdz);
+            this.Controls.Add(this.Check);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Check;
+        private System.Windows.Forms.Label Sprawdz;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutomatProgram
+{
+
+
+    public partial class Form1 : Form
+    {
+
+        Boolean a = true;
+        Boolean b = false;
+        Boolean c = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true;
+
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+
+            Color ColorOn = Color.Red;
+            Color ColorOff = Color.Black;
+
+
+            Pen pen1 = new Pen(Color.Black, 6);
+            pen1.StartCap = LineCap.RoundAnchor;
+            pen1.EndCap = LineCap.ArrowAnchor;
+            e.Graphics.DrawLine(pen1, 50, 175, 100, 175);
+
+            if (a == false)
+            {
+                Pen circle1 = new Pen(Color.Black, 3);
+                Rectangle rect = new Rectangle(110, 150, 50, 50);
+                e.Graphics.DrawEllipse(circle1, rect);
+            }
+            else
+            {
+                Pen circle1 = new Pen(Color.Red, 3);
+                Rectangle rect = new Rectangle(110, 150, 50, 50);
+                e.Graphics.DrawEllipse(circle1, rect);
+            }
+
+            Pen pen2 = new Pen(Color.Black, 6);
+            pen2.EndCap = LineCap.ArrowAnchor;
+            e.Graphics.DrawLine(pen2, 175, 175, 230, 175);
+
+            if (b == false)
+            { 
+                Pen circle2 = new Pen(Color.Black, 3);
+                Rectangle rect2 = new Rectangle(245, 150, 50, 50);
+                e.Graphics.DrawEllipse(circle2, rect2);
+            }
+            else
+            {
+                Pen circle2 = new Pen(Color.Red, 3);
+                Rectangle rect2 = new Rectangle(245, 150, 50, 50);
+                e.Graphics.DrawEllipse(circle2, rect2);
+            }
+
+            Pen pen3 = new Pen(Color.Black, 6);
+            pen3.EndCap = LineCap.ArrowAnchor;
+            e.Graphics.DrawLine(pen3, 310, 175, 360, 175);
+
+
+            if(c == false)
+            {
+                Pen circle3 = new Pen(Color.Black, 3);
+                Rectangle rect3 = new Rectangle(380, 150, 50, 50);
+                e.Graphics.DrawEllipse(circle3, rect3);
+
+                Pen circle4 = new Pen(Color.Black, 3);
+                Rectangle rect4 = new Rectangle(370, 140, 70, 70);
+                e.Graphics.DrawEllipse(circle4, rect4);
+            }
+            else
+            {
+                Pen circle3 = new Pen(Color.Red, 3);
+                Rectangle rect3 = new Rectangle(380, 150, 50, 50);
+                e.Graphics.DrawEllipse(circle3, rect3);
+
+                Pen circle4 = new Pen(Color.Red, 3);
+                Rectangle rect4 = new Rectangle(370, 140, 70, 70);
+                e.Graphics.DrawEllipse(circle4, rect4);
+            }
+
+
+
+        }
+
+
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+            Invalidate();
+
+        }
+
+        private void Check_Click(object sender, EventArgs e)
+        {
+            Boolean accept = false;
+            string input = textBox1.Text;
+
+            char[] characters = input.ToCharArray();
+
+            int licznik = 0;
+
+
+            for (int i = 0; i < input.Length; i++)
+            {
+
+
+                if (a == true && b == false && c == false)
+                {
+
+                    if(characters[i] == '1')
+                    {
+                        pictureBox3.Image = Image.FromFile(@"C:\Users\Albert\Desktop\AutomatProgram\LoppedRed.png");
+                        pictureBox2.Image = Image.FromFile(@"C:\Users\Albert\Desktop\AutomatProgram\Lopped2.png");
+                        pictureBox1.Image = Image.FromFile(@"C:\Users\Albert\Desktop\AutomatProgram\curved arrow2.png");
+                        a = true;
+                        b = false;
+                        c = false;
+                    }
+                    
+                    else if(characters[i] == '0')
+                    {
+                        pictureBox3.Image = Image.FromFile(@"C:\Users\Albert\Desktop\AutomatProgram\Lopped2.png");
+                        pictureBox2.Image = Image.FromFile(@"C:\Users\Albert\Desktop\AutomatProgram\Lopped2.png");
+                        pictureBox1.Image = Image.FromFile(@"C:\Users\Albert\Desktop\AutomatProgram\curved arrow2.png");
+                        a = false;
+                        b = true;
+                        c = false;
+                        Invalidate();
+                    }
+                    
+                    else
+                    {
+                        pictureBox3.Image = Image.FromFile(@"C:\Users\Albert\Desktop\AutomatProgram\Lopped2.png");
+                        pictureBox2.Image = Image.FromFile(@"C:\Users\Albert\Desktop\AutomatProgram\Lopped2.png");
+                        pictureBox1.Image = Image.FromFile(@"C:\Users\Albert\Desktop\AutomatProgram\curved arrow2.png");
+
+                        a = false;
+                        b = false;
+                        c = false;
+                        
+                    }
+
+                    licznik++;
+                    Application.DoEvents();
+                    Thread.Sleep(1000);
+                    Invalidate();
+
+
+                    if (licznik == input.Length)
+                    {
+                        Thread.Sleep(1000);
+                        pictureBox3.Image = Image.FromFile(@"C:\Users\Albert\Desktop\AutomatProgram\Lopped2.png");
+                        pictureBox2.Image = Image.FromFile(@"C:\Users\Albert\Desktop\AutomatProgram\Lopped2.png");
+                        pictureBox1.Image = Image.FromFile(@"C:\Users\Albert\Desktop\AutomatProgram\curved arrow2.png");
+
+                        a = true;
+                        b = false;
+                        c = false;
+
+                        characters = null;
+
+                        Invalidate();
+
+                        break;
+                    }
+
+                    continue;
+
+                }
+
+
+
+                if (b == true && a == false && c == false)
+                {
+
+                    if (characters[i] == '1')
+                    {
+                        pictureBox1.Image = Image.FromFile(@"C:\Users\Albert\Desktop\AutomatProgram\curvedArrowPic1Red.png");
+                        a = true;
+                        b = false;
+                        c = false;
+                    }
+
+                    else if (characters[i] == '0')
+                    {
+                        pictureBox3.Image = Image.FromFile(@"C:\Users\Albert\Desktop\AutomatProgram\Lopped2.png");
+                        a = false;
+                        b = false;
+                        c = true;
+
+                        accept = true;
+                    }
+
+                    else
+                    {
+                        pictureBox3.Image = Image.FromFile(@"C:\Users\Albert\Desktop\AutomatProgram\Lopped2.png");
+                        pictureBox2.Image = Image.FromFile(@"C:\Users\Albert\Desktop\AutomatProgram\Lopped2.png");
+                        pictureBox1.Image = Image.FromFile(@"C:\Users\Albert\Desktop\AutomatProgram\curved arrow2.png");
+
+                        a = false;
+                        b = false;
+                        c = false;
+                    }
+                    licznik++;
+                    Application.DoEvents();
+                    Thread.Sleep(1000);
+                    Invalidate();
+
+
+
+                    if (licznik == input.Length)
+                    {
+                        pictureBox3.Image = Image.FromFile(@"C:\Users\Albert\Desktop\AutomatProgram\Lopped2.png");
+                        pictureBox2.Image = Image.FromFile(@"C:\Users\Albert\Desktop\AutomatProgram\Lopped2.png");
+                        pictureBox1.Image = Image.FromFile(@"C:\Users\Albert\Desktop\AutomatProgram\curved arrow2.png");
+
+                        a = true;
+                        b = false;
+                        c = false;
+
+                        characters = null;
+
+                        Invalidate();
+
+                        break;
+                    }
+
+                    continue;
+
+                }
+
+
+                if (c == true && b == false && a == false)
+                {
+
+
+                    if (characters[i] == '1')
+                    {
+                        pictureBox2.Image = Image.FromFile(@"C:\Users\Albert\Desktop\AutomatProgram\LoppedRed.png");
+                        a = false;
+                        b = false;
+                        c = true;
+                    }
+
+                    else if (characters[i] == '0')
+                    {
+                        pictureBox2.Image = Image.FromFile(@"C:\Users\Albert\Desktop\AutomatProgram\LoppedRed.png");
+                        a = false;
+                        b = false;
+                        c = true;
+
+                    }
+
+                    else
+                    {
+                        pictureBox3.Image = Image.FromFile(@"C:\Users\Albert\Desktop\AutomatProgram\Lopped2.png");
+                        pictureBox2.Image = Image.FromFile(@"C:\Users\Albert\Desktop\AutomatProgram\Lopped2.png");
+                        pictureBox1.Image = Image.FromFile(@"C:\Users\Albert\Desktop\AutomatProgram\curved arrow2.png");
+
+                        a = false;
+                        b = false;
+                        c = false;
+                    }
+
+
+
+                    accept = true;
+                    licznik++;
+                    Application.DoEvents();
+                    Thread.Sleep(1000);
+                    Invalidate();
+
+
+
+
+                    continue;
+                }
+
+            }
+
+            if (accept == true && licznik == input.Length)
+            {
+                pictureBox3.Image = Image.FromFile(@"C:\Users\Albert\Desktop\AutomatProgram\Lopped2.png");
+                pictureBox2.Image = Image.FromFile(@"C:\Users\Albert\Desktop\AutomatProgram\Lopped2.png");
+                pictureBox1.Image = Image.FromFile(@"C:\Users\Albert\Desktop\AutomatProgram\curved arrow2.png");
+
+                a = true;
+                b = false;
+                c = false;
+
+                characters = null;
+
+
+                MessageBox.Show("Program zaakceptował słowo", "Komunikat", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Invalidate();
+
+            }
+
+        }
+    }
+}
